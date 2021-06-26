8501555816d823b046cfa6b25da06519e7ca5e3e
Hanna Lee <8annahxxl@gmail.com>
2021-06-26 23:53:18 +0900

    Refactor : 더미 데이터 추가로 인한 컴포넌트 수정 #1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // eslint-disable
-import "./App.css";
 import { useState } from "react";
+import "./App.css";
+import { posts } from "./data";
 
 function App() {
-  let title = "리액트 블로그 만들기";
-  let created = "Jun. 26, 2021";
-  let [likes, setLikes] = useState(0);
-  let content =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+  let [likes, setLikes] = useState(posts.map((post) => post.likes));
   let [modalStatus, setModalStatus] = useState(false);
+  let [postIdx, setPostIdx] = useState(0);
 
   return (
     <div className="App">
@@ -19,19 +17,23 @@ function App() {
       <div className="title">All</div>
 
       <div className="list-container">
-        <Posts
-          title={title}
-          created={created}
-          likes={likes}
-          setLikes={setLikes}
-          setModalStatus={setModalStatus}
-        ></Posts>
+        {posts.map((post, index) => {
+          return (
+            <Posts
+              postIdx={index}
+              posts={posts}
+              likes={likes}
+              setLikes={setLikes}
+              setModalStatus={setModalStatus}
+              setPostIdx={setPostIdx}
+            ></Posts>
+          );
+        })}
       </div>
       {modalStatus ? (
         <Modal
-          title={title}
-          created={created}
-          content={content}
+          posts={posts}
+          postIdx={postIdx}
           setModalStatus={setModalStatus}
         ></Modal>
       ) : null}
@@ -45,22 +47,27 @@ function Posts(props) {
       <div
         className="post__title"
         onClick={() => {
+          props.setPostIdx(props.postIdx);
           props.setModalStatus(true);
         }}
       >
-        {props.title}
+        {props.posts[props.postIdx].title}
       </div>
       <div className="post__meta">
-        <div className="created">Created at {props.created}</div>
+        <div className="created">
+          Created at {props.posts[props.postIdx].created}
+        </div>
         <div className="likes">
           <span
             onClick={() => {
-              props.setLikes(props.likes + 1);
+              let newLikes = [...props.likes];
+              newLikes[props.postIdx] += 1;
+              props.setLikes(newLikes);
             }}
           >
             ❤
-          </span>{" "}
-          {props.likes}
+          </span>
+          {props.likes[props.postIdx]}
         </div>
       </div>
     </div>
@@ -79,9 +86,13 @@ function Modal(props) {
         >
           닫기
         </div>
-        <div className="modal__created">{props.created}</div>
-        <div className="modal__title">{props.title}</div>
-        <div className="modal__content">{props.content}</div>
+        <div className="modal__created">
+          {props.posts[props.postIdx].created}
+        </div>
+        <div className="modal__title">{props.posts[props.postIdx].title}</div>
+        <div className="modal__content">
+          {props.posts[props.postIdx].content}
+        </div>
       </div>
     </div>
   );
