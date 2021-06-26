8fb7a72e3cea1a1ca7cd2a9d5c70c5f82faffdaa
Hanna Lee <8annahxxl@gmail.com>
2021-06-26 19:25:56 +0900

    Add : 모달 창 열기/닫기 기능 구현, 게시글 & 모달 컴포넌트로 재생성 #1

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
   let [likes, setLikes] = useState(0);
   let content =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
-
-  function increaseLikes() {
-    setLikes(likes + 1);
-  }
+  let [modalStatus, setModalStatus] = useState(false);
 
   return (
     <div className="App">
@@ -22,24 +19,69 @@ function App() {
       <div className="title">All</div>
 
       <div className="list-container">
-        <div className="post">
-          <div className="post__title">{title}</div>
-          <div className="post__meta">
-            <div className="created">Created at {created}</div>
-            <div className="likes">
-              <span onClick={increaseLikes}>❤</span> {likes}
-            </div>
-          </div>
+        <Posts
+          title={title}
+          created={created}
+          likes={likes}
+          setLikes={setLikes}
+          setModalStatus={setModalStatus}
+        ></Posts>
+      </div>
+      {modalStatus ? (
+        <Modal
+          title={title}
+          created={created}
+          content={content}
+          setModalStatus={setModalStatus}
+        ></Modal>
+      ) : null}
+    </div>
+  );
+}
+
+function Posts(props) {
+  return (
+    <div className="post">
+      <div
+        className="post__title"
+        onClick={() => {
+          props.setModalStatus(true);
+        }}
+      >
+        {props.title}
+      </div>
+      <div className="post__meta">
+        <div className="created">Created at {props.created}</div>
+        <div className="likes">
+          <span
+            onClick={() => {
+              props.setLikes(props.likes + 1);
+            }}
+          >
+            ❤
+          </span>{" "}
+          {props.likes}
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="modal-container hidden">
-        <div className="modal">
-          <div className="modal__btn--close">닫기</div>
-          <div className="modal__created">{created}</div>
-          <div className="modal__title">{title}</div>
-          <div className="modal__content">{content}</div>
+function Modal(props) {
+  return (
+    <div className="modal-container hidden">
+      <div className="modal">
+        <div
+          className="modal__btn--close"
+          onClick={() => {
+            props.setModalStatus(false);
+          }}
+        >
+          닫기
         </div>
+        <div className="modal__created">{props.created}</div>
+        <div className="modal__title">{props.title}</div>
+        <div className="modal__content">{props.content}</div>
       </div>
     </div>
   );
